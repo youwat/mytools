f8b7c8fdc6f51ff24ec90eb606ad6cb20add107e
Yu Watanabe <ywatanabe@techloco.co.jp>
2015-11-10 15:28:54 +0900

    テストコード削除

--- app/Http/Controllers/topController.php
+++ app/Http/Controllers/topController.php
@@ -1,15 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Yu
- * Date: 2015/11/09
- * Time: 16:43
- */
-namespace App\Http\Controllers\Controller;
-use App\Http\Controllers\Controller;
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
 
 class TopController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return view('top');
